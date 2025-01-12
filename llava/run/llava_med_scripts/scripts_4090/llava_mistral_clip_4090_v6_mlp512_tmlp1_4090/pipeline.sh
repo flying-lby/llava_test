6579d382dbf820e4d06c2cba457dc68b6c5507a2
flying-lby <2230232178@qq.com>
2025-01-12 21:52:28 +0800

    update_1_11

---
+++ llava/run/llava_med_scripts/scripts_4090/llava_mistral_clip_4090_v6_mlp512_tmlp1_4090/pipeline.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+###
+ # @Author: fly
+ # @Date: 2024-08-24 14:44:49
+ # @FilePath: /llava_med/LLaVA-Med/llava/run/llava_med_scripts/scripts_4090/llava_mistral_clip_4090_v4_mlp512_tmlp1_4090/pipeline.sh
+ # @Description: 
+### 
+
+# ========================
+# Training
+# ========================
+echo "Starting training process..."
+
+deepspeed train/train_mem.py \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 5e-5 \
+    --deepspeed train/zero3.json \
+    --model_name_or_path /srv/lby/llava_med/llava-med-v1.5-mistral-7b \
+    --version v1 \
+    --data_path ./data/chest_xray/llm_classify_mimic_file_clip.json \
+    --image_folder /srv/lby/physionet.org/files/mimic-cxr-jpg/2.0.0/files \
+    --vision_tower /srv/lby/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --mis_mlp_lr 5e-4 \
+    --output_dir ./checkpoints/llava-lora-new-clip-v6 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 50000 \
+    --save_total_limit 1 \
+    --learning_rate 5e-6 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 2 \
+    --lazy_preprocess True \
+    --report_to wandb \
+    --ncls_count 4 \
+    --hidden_dim 1024 \
+    --output_dim 4096 \
+    --mlp_type 0 \
+    --loss_threshold 0.5 \
+    --temperature 0.05 \
+    --use_local_loss True \
+    --feature_layer 2 \
+    --special_tokens_mlp_type 1 \
+    --use_ca_loss False
+
+if [ $? -ne 0 ]; then
+    echo "Training failed. Exiting..."
+    exit 1
+fi
+echo "Training completed successfully."
+
+# ========================
+# Merge LoRA
+# ========================
+echo "Starting merge process..."
+
+python -m llava.run.train.merge_lora_weights \
+    --model-path ./checkpoints/llava-lora-new-clip-v6 \
+    --model-base /srv/lby/llava_med/llava-med-v1.5-mistral-7b \
+    --save-model-path /srv/lby/llava_med/checkpoints/llava-mistral_new_clip_v6 \
+    --ncls_count 4 \
+    --hidden_dim 1024 \
+    --output_dim 4096 \
+    --mlp_type 0 \
+    --loss_threshold 0.5 \
+    --temperature 0.05 \
+    --use_local_loss True \
+    --feature_layer 2 \
+    --special_tokens_mlp_type 1 \
+    --use_ca_loss False
+
+
+if [ $? -ne 0 ]; then
+    echo "Merge failed. Exiting..."
+    exit 1
+fi
+echo "Merge completed successfully."
+
+python -m llava.run.eval.eval_classify \
+    --model-path /srv/lby/llava_med/checkpoints/llava-mistral_new_clip_v6 \
+    --result-file ./result/R4090/llava-mistral_new_clip_v6/Chest_Xray_classify.txt \
+    --question-file ./data/chest_xray/Chest-X-ray_llava_val.jsonl \
+    --image-folder "/srv/lby" \
+    --conv-mode vicuna_v1 \
+    --ncls_count 4 \
+    --hidden_dim 1024 \
+    --output_dim 4096 \
+    --mlp_type 0 \
+    --loss_threshold 0.5 \
+    --temperature 0.05 \
+    --use_local_loss True \
+    --feature_layer 2 \
+    --special_tokens_mlp_type 1 \
+    --use_ca_loss False
---
+++ llava/run/llava_med_scripts/scripts_A100/2025-01-11/llava_mistral_clip_A100_v4_mlp5_llm_dataset_2epoch_2A100/train_merge_lora.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+###
+ # @Author: fly
+ # @Date: 2024-12-26 16:50:23
+ # @FilePath: /llava_med/LLaVA-Med/llava/run/llava_med_scripts/scripts_A100/2025-01-11/llava_mistral_clip_A100_v4_mlp5_llm_dataset_2epoch_2A100/train_merge_lora.sh
+ # @Description: 
+### 
+
+# ========================
+# Training 
+# mlp 3
+# ========================
+echo "Starting training process..."
+
+deepspeed train/train_mem.py \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-6 \
+    --deepspeed train/zero3.json \
+    --model_name_or_path /mnt/nlp-ali/usr/huangwenxuan/home/official_llava_med/llava-med-v1.5-mistral-7b \
+    --version v1 \
+    --data_path ./data/chest_xray/llm_classify_mimic_file_clip.json \
+    --image_folder /mnt/nlp-ali/usr/huangwenxuan/home/dataset/srv/lby/physionet.org/files/mimic-cxr-jpg/2.0.0/files \
+    --vision_tower /mnt/nlp-ali/usr/huangwenxuan/home/vision_tower/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --mis_mlp_lr 2e-5 \
+    --output_dir /mnt/nlp-ali/usr/huangwenxuan/home/zijie_ali/libangyan/checkpoints/llava-lora-new-clip-A100-version4_1_11 \
+    --num_train_epochs 2 \
+    --per_device_train_batch_size 64 \
+    --per_device_eval_batch_size 64 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 50000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 8 \
+    --lazy_preprocess True \
+    --report_to wandb \
+    --ncls_count 4 \
+    --hidden_dim 1024 \
+    --output_dim 4096 \
+    --mlp_type 5 \
+    --loss_threshold 0.4 \
+    --temperature 0.05 \
+    --use_local_loss True \
+    --feature_layer 2 \
+    --special_tokens_mlp_type 1 \
+    --use_ca_loss False
+  
+    
+
+if [ $? -ne 0 ]; then
+    echo "Training failed. Exiting..."
+    exit 1
+fi
+echo "Training completed successfully."
+
+# ========================
+# Merge LoRA
+# ========================
+echo "Starting merge process..."
+
+python -m llava.run.train.merge_lora_weights \
+    --model-path /mnt/nlp-ali/usr/huangwenxuan/home/zijie_ali/libangyan/checkpoints/llava-lora-new-clip-A100-version4_1_11 \
+    --model-base /mnt/nlp-ali/usr/huangwenxuan/home/official_llava_med/llava-med-v1.5-mistral-7b \
+    --save-model-path /mnt/nlp-ali/usr/huangwenxuan/home/zijie_ali/libangyan/checkpoints/llava_mistral_new_clip_a100_version4_1_11 \
+    --ncls_count 4 \
+    --hidden_dim 1024 \
+    --output_dim 4096 \
+    --mlp_type 5 \
+    --loss_threshold 0.4 \
+    --temperature 0.05 \
+    --use_local_loss True \
+    --feature_layer 2 \
+    --special_tokens_mlp_type 1 \
+    --use_ca_loss False
+
+if [ $? -ne 0 ]; then
+    echo "Merge failed. Exiting..."
+    exit 1
+fi
+echo "Merge completed successfully."
