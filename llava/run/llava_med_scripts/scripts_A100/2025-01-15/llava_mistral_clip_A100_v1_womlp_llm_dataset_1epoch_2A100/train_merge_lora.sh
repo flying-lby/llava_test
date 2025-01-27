8885ca1b5ed3034165b0bf4d12226801b290d3c4
flying-lby <2230232178@qq.com>
2025-01-27 22:42:25 +0800

    fix merge bug

--- llava/run/llava_med_scripts/scripts_A100/2025-01-15/llava_mistral_clip_A100_v1_womlp_llm_dataset_1epoch_2A100/train_merge_lora.sh
+++ llava/run/llava_med_scripts/scripts_A100/2025-01-15/llava_mistral_clip_A100_v1_womlp_llm_dataset_1epoch_2A100/train_merge_lora.sh
@@ -10,62 +10,62 @@
 # Training 
 # mlp 3
 # ========================
-echo "Starting training process..."
+# echo "Starting training process..."
 
-deepspeed train/train_mem.py \
-    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-6 \
-    --deepspeed train/zero3.json \
-    --model_name_or_path /mnt/nlp-ali/usr/huangwenxuan/home/official_llava_med/llava-med-v1.5-mistral-7b \
-    --version v1 \
-    --data_path ./data/chest_xray/llm_classify_mimic_file_clip.json \
-    --image_folder /mnt/nlp-ali/usr/huangwenxuan/home/dataset/srv/lby/physionet.org/files/mimic-cxr-jpg/2.0.0/files \
-    --vision_tower /mnt/nlp-ali/usr/huangwenxuan/home/vision_tower/clip-vit-large-patch14-336 \
-    --mm_projector_type mlp2x_gelu \
-    --mm_vision_select_layer -2 \
-    --mm_use_im_start_end False \
-    --mm_use_im_patch_token False \
-    --image_aspect_ratio pad \
-    --group_by_modality_length True \
-    --bf16 True \
-    --mis_mlp_lr 2e-5 \
-    --output_dir /mnt/nlp-ali/usr/huangwenxuan/home/zijie_ali/libangyan/checkpoints/llava-lora-new-clip-A100-version1_1_15 \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 64 \
-    --per_device_eval_batch_size 64 \
-    --gradient_accumulation_steps 1 \
-    --evaluation_strategy "no" \
-    --save_strategy "steps" \
-    --save_steps 50000 \
-    --save_total_limit 1 \
-    --learning_rate 2e-5 \
-    --weight_decay 0. \
-    --warmup_ratio 0.03 \
-    --lr_scheduler_type "cosine" \
-    --logging_steps 1 \
-    --tf32 True \
-    --model_max_length 2048 \
-    --gradient_checkpointing True \
-    --dataloader_num_workers 8 \
-    --lazy_preprocess True \
-    --report_to wandb \
-    --ncls_count 4 \
-    --hidden_dim 1024 \
-    --output_dim 4096 \
-    --mlp_type 0 \
-    --loss_threshold 0.4 \
-    --temperature 0.05 \
-    --use_local_loss True \
-    --feature_layer 2 \
-    --special_tokens_mlp_type 1 \
-    --use_ca_loss False
+# deepspeed train/train_mem.py \
+#     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-6 \
+#     --deepspeed train/zero3.json \
+#     --model_name_or_path /mnt/nlp-ali/usr/huangwenxuan/home/official_llava_med/llava-med-v1.5-mistral-7b \
+#     --version v1 \
+#     --data_path ./data/chest_xray/llm_classify_mimic_file_clip.json \
+#     --image_folder /mnt/nlp-ali/usr/huangwenxuan/home/dataset/srv/lby/physionet.org/files/mimic-cxr-jpg/2.0.0/files \
+#     --vision_tower /mnt/nlp-ali/usr/huangwenxuan/home/vision_tower/clip-vit-large-patch14-336 \
+#     --mm_projector_type mlp2x_gelu \
+#     --mm_vision_select_layer -2 \
+#     --mm_use_im_start_end False \
+#     --mm_use_im_patch_token False \
+#     --image_aspect_ratio pad \
+#     --group_by_modality_length True \
+#     --bf16 True \
+#     --mis_mlp_lr 2e-5 \
+#     --output_dir /mnt/nlp-ali/usr/huangwenxuan/home/zijie_ali/libangyan/checkpoints/llava-lora-new-clip-A100-version1_1_15 \
+#     --num_train_epochs 1 \
+#     --per_device_train_batch_size 64 \
+#     --per_device_eval_batch_size 64 \
+#     --gradient_accumulation_steps 1 \
+#     --evaluation_strategy "no" \
+#     --save_strategy "steps" \
+#     --save_steps 50000 \
+#     --save_total_limit 1 \
+#     --learning_rate 2e-5 \
+#     --weight_decay 0. \
+#     --warmup_ratio 0.03 \
+#     --lr_scheduler_type "cosine" \
+#     --logging_steps 1 \
+#     --tf32 True \
+#     --model_max_length 2048 \
+#     --gradient_checkpointing True \
+#     --dataloader_num_workers 8 \
+#     --lazy_preprocess True \
+#     --report_to wandb \
+#     --ncls_count 4 \
+#     --hidden_dim 1024 \
+#     --output_dim 4096 \
+#     --mlp_type 0 \
+#     --loss_threshold 0.4 \
+#     --temperature 0.05 \
+#     --use_local_loss True \
+#     --feature_layer 2 \
+#     --special_tokens_mlp_type 1 \
+#     --use_ca_loss False
   
     
 
-if [ $? -ne 0 ]; then
-    echo "Training failed. Exiting..."
-    exit 1
-fi
-echo "Training completed successfully."
+# if [ $? -ne 0 ]; then
+#     echo "Training failed. Exiting..."
+#     exit 1
+# fi
+# echo "Training completed successfully."
 
 # ========================
 # Merge LoRA
