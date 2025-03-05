5ce2954d58342f94480214882a36d48587edfe61
flying-lby <2230232178@qq.com>
2025-03-06 05:25:44 +0800

    11

--- llava/run/llava_med_scripts/ex_scripts_A100/3_5/siim/llava_mistral_clip_siim_100_2A100/train_merge_lora.sh
+++ llava/run/llava_med_scripts/ex_scripts_A100/3_5/siim/llava_mistral_clip_siim_100_2A100/train_merge_lora.sh
@@ -29,7 +29,7 @@ deepspeed train/clip_train_mem.py \
     --bf16 True \
     --mis_mlp_lr 2e-5 \
     --output_dir /mnt/nlp-ali/usr/huangwenxuan/home/zijie_ali/libangyan/checkpoints/llava-lora-siim-A100-version100_3_5 \
-    --num_train_epochs 15 \
+    --num_train_epochs 12 \
     --per_device_train_batch_size 64 \
     --per_device_eval_batch_size 64 \
     --gradient_accumulation_steps 1 \
