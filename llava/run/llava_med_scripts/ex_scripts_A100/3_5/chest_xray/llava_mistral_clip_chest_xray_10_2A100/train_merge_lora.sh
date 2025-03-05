1f1cc1a292552e7130eb1ca3dc8b0817c32423e3
flying-lby <2230232178@qq.com>
2025-03-06 04:38:57 +0800

    11

--- llava/run/llava_med_scripts/ex_scripts_A100/3_5/chest_xray/llava_mistral_clip_chest_xray_10_2A100/train_merge_lora.sh
+++ llava/run/llava_med_scripts/ex_scripts_A100/3_5/chest_xray/llava_mistral_clip_chest_xray_10_2A100/train_merge_lora.sh
@@ -29,7 +29,7 @@ deepspeed train/clip_train_mem.py \
     --bf16 True \
     --mis_mlp_lr 2e-5 \
     --output_dir /mnt/nlp-ali/usr/huangwenxuan/home/zijie_ali/libangyan/checkpoints/llava-lora-chest_xray-A100-version10_3_5 \
-    --num_train_epochs 8 \
+    --num_train_epochs 12 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 32 \
     --gradient_accumulation_steps 1 \
